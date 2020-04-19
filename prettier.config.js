c590e8f76a470e46a1b42e8394f437b35230e707
Christopher Pitt <cgpitt@gmail.com>
2020-04-19 14:18:53 +0200

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    printWidth: 120,
+    tabWidth: 4,
+    useTabs: false,
+    semi: false,
+    singleQuote: true,
+    quoteProps: 'as-needed',
+    jsxSingleQuote: false,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    arrowParens: 'avoid',
+}
